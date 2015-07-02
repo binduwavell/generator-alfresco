3fb89d35745b281a6935f6b34fd1c94d302af7d6
Bindu Wavell <github@spamguard.wavell.net>
2015-07-02 00:08:20 +0000

    Broke sdkVersions datastructure out into a module that we can re-use from sub-modules if necessary.

--- app/index.js
+++ app/index.js
@@ -17,67 +17,7 @@ module.exports = yeoman.generators.Base.extend({
     this.out = require('./app-output.js')(this);
     this.pkg = require('../package.json');
     this.config.set('generatorVersion', this.pkg.version);
-    this.sdkVersions = {
-      "2.1.0": {
-        archetypeGroupId: 'org.alfresco.maven.archetype',
-        archetypeArtifactId: 'alfresco-allinone-archetype',
-        archetypeVersion: '2.1.0',
-        promptForProjectPackage: true,
-        supportedJavaVersions: '^1.8.0',
-        supportedMavenVersions: '^3.2.5',
-        removeSamplesScript: [
-          {
-            cmd: 'removeFolders', 
-            paths:  [
-              'repo-amp/src/main/java/${projectPackagePath}/demoamp',
-              'repo-amp/src/test/java/${projectPackagePath}/demoamp'
-          	]
-          },
-          {
-            cmd: 'removeFiles', 
-            paths: [
-              'repo-amp/src/main/amp/web/css/demoamp.css',
-              'repo-amp/src/main/amp/web/jsp/demoamp.jsp',
-              'repo-amp/src/main/amp/web/scripts/demoamp.js',
-              'repo-amp/src/main/amp/config/alfresco/extension/templates/webscripts/helloworld.get.desc.xml',
-              'repo-amp/src/main/amp/config/alfresco/extension/templates/webscripts/helloworld.get.html.ftl',
-              'repo-amp/src/main/amp/config/alfresco/extension/templates/webscripts/helloworld.get.js',
-              'repo-amp/src/main/amp/config/alfresco/module/repo-amp/webscripts/helloworld.get.js',
-              'repo-amp/src/main/amp/config/alfresco/module/repo-amp/context/bootstrap-context.xml',
-              'repo-amp/src/main/amp/config/alfresco/module/repo-amp/context/service-context.xml',
-              'repo-amp/src/main/amp/config/alfresco/module/repo-amp/context/webscript-context.xml',
-          	]
-          },
-          {
-            cmd: 'removeContextFileImports', 
-            contextFile: 'repo-amp/src/main/amp/config/alfresco/module/repo-amp/module-context.xml',
-            imports: [
-              'classpath:alfresco/module/${project.artifactId}/context/service-context.xml',
-              'classpath:alfresco/module/${project.artifactId}/context/bootstrap-context.xml',
-              'classpath:alfresco/module/${project.artifactId}/context/webscript-context.xml',
-            ]
-          },
-        ],
-      },
-      "2.0.0": {
-        archetypeGroupId: 'org.alfresco.maven.archetype',
-        archetypeArtifactId: 'alfresco-allinone-archetype',
-        archetypeVersion: '2.0.0',
-        promptForProjectPackage: false,
-        supportedJavaVersions: '^1.7.0',
-        supportedMavenVersions: '^3.0.5',
-      },
-      "local": {
-        archetypeGroupId: "org.alfresco.maven.archetype",
-        archetypeArtifactId: "alfresco-allinone-archetype",
-        archetypeVersion: "2.1.0-SNAPSHOT",
-        archetypeCatalog: 'local',
-        promptForArchetypeVersion: true,
-        promptForProjectPackage: true,
-        supportedJavaVersions: '^1.8.0',
-        supportedMavenVersions: '^3.2.5',
-      }
-    };
+    this.sdkVersions = require("./sdk-versions.js");
     this.config.defaults({
       sdkVersion: '2.1.0',
       projectGroupId: 'org.alfresco',
