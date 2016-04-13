4952dc00b6f9db416d181b1cf2476897b7aa5721
Bindu Wavell <bindu@ziaconsulting.com>
2016-04-12 19:55:04 -0600

    updated explode-alf-sources.sh to target root
    fixes #58

--- generators/app/templates/scripts/explode-alf-sources.sh
+++ generators/app/templates/scripts/explode-alf-sources.sh
@@ -6,11 +6,11 @@ CWD=$( pwd )
 cd "$DIR"
 
 cd ../source_templates/<%= sdkVersionPrefix %>repo-amp
-mvn dependency:unpack-dependencies <%= enterpriseFlag %> -Dclassifier=sources -DoutputDirectory=../exploded
+mvn dependency:unpack-dependencies <%= enterpriseFlag %> -Dclassifier=sources -DoutputDirectory=../../exploded
 rm -rf target
 
 cd ../source_templates/<%= sdkVersionPrefix %>share-amp
-mvn dependency:unpack-dependencies <%= enterpriseFlag %> -Dclassifier=sources -DoutputDirectory=../exploded
+mvn dependency:unpack-dependencies <%= enterpriseFlag %> -Dclassifier=sources -DoutputDirectory=../../exploded
 rm -rf target
 
 cd "$CWD"
