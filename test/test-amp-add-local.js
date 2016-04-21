b051b59896992b82fe142588ba09455fb940c463
Bindu Wavell <bindu@ziaconsulting.com>
2016-04-21 15:57:32 -0600

    add extra isObject check when reading amp props
    added a test that reproduced the issue Jeff reported and then
    added a check that resolved the issue.
    
    fixes #77

--- test/test-amp-add-local.js
+++ test/test-amp-add-local.js
@@ -108,6 +108,40 @@ describe('generator-alfresco:amp-add-local', function () {
     });
   });
 
+  describe('installing an empty local repo amp using prompts', function () {
+    before(function (done) {
+      var ampSrc = path.join(__dirname, 'fixtures/empty-amp.amp');
+      var ampPath = path.join(osTempDir, 'amps/empty-amp.amp');
+      fs.writeFileSync(ampPath, fs.readFileSync(ampSrc));
+      helpers.run(path.join(__dirname, '../generators/amp-add-local'))
+        // generator will create a temp directory and make sure it's empty
+        .inTmpDir(function () {
+          // HACK: we want our test to run inside the previously generated
+          // directory and we don't want it to be empty, so this is a hack
+          // for that.
+          process.chdir(osTempDir);
+        })
+        .withOptions({
+          'force': true, // tests can't handle conflicts
+        })
+        .withPrompts({
+          'path': 'amps/empty-amp.amp',
+          'groupId': 'com.empty',
+          'artifactId': 'empty-amp',
+          'version': '1.0.0-SNAPSHOT',
+        })
+        .on('end', done);
+    });
+
+    it('references amp in share/pom.xml', function () {
+      assert.fileContent([
+        [path.join(osTempDir, 'repo/pom.xml'), /<groupId>com.empty</],
+        [path.join(osTempDir, 'repo/pom.xml'), /<artifactId>empty-amp</],
+        [path.join(osTempDir, 'repo/pom.xml'), /<version>1.0.0-SNAPSHOT</],
+      ]);
+    });
+  });
+
   describe('attempting to install a local amp using invalid options when all amps are accounted for', function () {
     // if you skip the previous test so that the share amp is not installed
     // prior to this code running this will fail with nasty errors related
