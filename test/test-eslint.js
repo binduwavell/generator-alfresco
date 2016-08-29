831c9c13088c798f1ecd1a41f2484b84fb7e44d5
Bindu Wavell <bindu@ziaconsulting.com>
2016-08-28 18:06:43 -0600

    exclude eslint tests when node < 4.4.0

--- test/test-eslint.js
+++ test/test-eslint.js
@@ -1,27 +1,28 @@
 'use strict';
 /* eslint-env node, mocha */
-var lint = require('mocha-eslint');
 var semver = require('semver');
-
-var paths = [
-  'generators/*.js',
-  'generators/*/*.js',
-  'test/*.js',
-];
-
-var options = {
-  // Specify style of output
-  formatter: 'compact',
-  // Only display warnings if a test is failing
-  alwaysWarn: false,
-  // Increase the timeout of the test if linting takes to long
-  timeout: 5000,
-};
-
-// Run the tests if node is new enough to run eslint
 var versions = process.versions;
-if (semver.gte(versions.node, '4.4.0')) {
-  lint(paths, options);
-} else {
+if (semver.lt(versions.node, '4.4.0')) {
   console.warn('Not running eslint because ' + versions.node + ' is not supported by eslint, min node version is 4.4.0');
+  process.exit(0);
+} else {
+  var lint = require('mocha-eslint');
+
+  var paths = [
+    'generators/*.js',
+    'generators/*/*.js',
+    'test/*.js',
+  ];
+
+  var options = {
+    // Specify style of output
+    formatter: 'compact',
+    // Only display warnings if a test is failing
+    alwaysWarn: false,
+    // Increase the timeout of the test if linting takes to long
+    timeout: 5000,
+  };
+
+  // Run the tests if node is new enough to run eslint
+  lint(paths, options);
 }
