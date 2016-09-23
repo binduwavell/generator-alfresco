4032ebbd5a914012cc8d89014f838198a70fe602
Bindu Wavell <github@spamguard.wavell.net>
2016-09-23 16:01:32 -0600

    Openjdk (#138)
    * add support for openjdk
    
    fixes #130

--- generators/common/dependency-versions.js
+++ generators/common/dependency-versions.js
@@ -1,22 +1,35 @@
 'use strict';
+var _ = require('lodash');
 var deasync = require('deasync');
+var fs = require('fs');
 var path = require('path');
 var spawn = require('cross-spawn');
 var split = require('split');
+var debug = require('debug')('generator-alfresco:dependency-version');
 
 module.exports.getJavaVersion = function (cmd) {
   if (!cmd) {
     if (process.env.JAVA_HOME) {
       cmd = path.join(process.env.JAVA_HOME, '/bin/java');
     } else {
-      cmd = 'java';
+      var defaultCmds = ['/bin/java', '/usr/bin/java', '/usr/local/bin/java'];
+      cmd = _.find(defaultCmds, function (c) {
+        var e = fs.existsSync(c);
+        debug('Checking if Java binary exists at: ' + c + ', ' + e);
+        return e;
+      });
+      if (cmd === undefined) {
+        cmd = 'java';
+      }
     }
   }
+  debug('Using java cmd: ' + cmd);
   var args = ['-version'];
 
   // java version "1.7.0_75"
-  var match = this.getRegExpMatchFromProcessOutput(cmd, args, /^java version "([^"]*)"$/);
+  var match = this.getRegExpMatchFromProcessOutput(cmd, args, /^(?:java|openjdk) version "([^"]*)"$/);
   if (match) {
+    debug('Java Version Match: ' + match[1]);
     return match[1];
   } else {
     return undefined;
@@ -28,7 +41,15 @@ module.exports.getMavenVersion = function (cmd) {
     if (process.env.M2_HOME) {
       cmd = path.join(process.env.M2_HOME, '/bin/mvn');
     } else {
-      cmd = 'mvn';
+      var defaultCmds = ['/bin/mvn', '/usr/bin/mvn', '/usr/local/bin/mvn'];
+      cmd = _.find(defaultCmds, function (c) {
+        var e = fs.existsSync(c);
+        debug('Checking if Maven binary exists at: ' + c + ', ' + e);
+        return e;
+      });
+      if (cmd === undefined) {
+        cmd = 'mvn';
+      }
     }
   }
   var args = ['-version'];
@@ -36,6 +57,7 @@ module.exports.getMavenVersion = function (cmd) {
   // Apache Maven 3.3.1 (cab6659f9874fa96462afef40fcf6bc033d58c1c; 2015-03-13T13:10:27-07:00)
   var match = this.getRegExpMatchFromProcessOutput(cmd, args, /^Apache Maven ([0-9.]*)/);
   if (match) {
+    debug('Maven Version Match: ' + match[1]);
     return match[1];
   } else {
     return undefined;
@@ -48,6 +70,7 @@ module.exports.getRegExpMatchFromProcessOutput = function (cmd, args, re) {
 
   // if line matches regex return line
   var processLine = function (line) {
+    debug('Checking cmd output: ' + line);
     var match = line.match(re);
     if (match !== null) {
       retv = match;
