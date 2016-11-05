b610a4a0ee4d17b9ef44e2cc03775ccfbef1d2fa
Bindu Wavell <bindu@ziaconsulting.com>
2016-11-05 09:31:27 -0600

    Starting work on testing base generator

---
+++ test/test-base-generator.js
@@ -0,0 +1,35 @@
+'use strict';
+/* eslint-env node, mocha */
+/*
+var assert = require('yeoman-assert');
+var helpers = require('yeoman-test');
+var os = require('os');
+var path = require('path');
+var BaseGenerator = require('./base-generator.js');
+
+TestGenerator = BaseGenerator.extend({
+  constructor: function () {
+    BaseGenerator.apply(this, arguments);
+  }
+})
+
+describe('generator-alfresco:amp-add-common', function () {
+  this.timeout(30000);
+
+  // We need a test project setup before we begin
+  before(function () {
+    return helpers.run(path.join(__dirname, '../generators/app'))
+      .inDir(osTempDir)
+      .withOptions({ 'skip-install': true })
+      .withPrompts({
+        sdkVersion: '2.1.1',
+        projectArtifactId: 'temp-common-test',
+        removeDefaultSourceAmps: false,
+      })
+      .toPromise();
+  });
+
+  it('starts with a basic project', function () {
+  });
+})
+*/
