79da8b27a9671c4145f6c51786defcc23905518f
Bindu Wavell <bindu@ziaconsulting.com>
2017-12-17 20:53:51 -0700

    Simplify generator delegation for amp-add-source

--- generators/amp-add-source/index.js
+++ generators/amp-add-source/index.js
@@ -1,40 +1,29 @@
 'use strict';
 const chalk = require('chalk');
 const debug = require('debug')('generator-alfresco:amp-add-source');
-const SubGenerator = require('../subgenerator.js');
+const Generator = require('yeoman-generator');
 
 const DELEGATED_NAMESPACE = 'alfresco:module-add-source';
 
-class AmpAddSourceSubGenerator extends SubGenerator {
+class AmpAddSourceSubGenerator extends Generator {
   constructor (args, opts) {
     super(args, opts);
-
-    this.prompts = [];
-
-    this.setupArgumentsAndOptions(this.prompts);
+    this.out = require('generator-alfresco-common').generator_output(this);
   }
 
   help () {
-    const Generator = require('yeoman-generator');
     const subgen = this.env.create(DELEGATED_NAMESPACE);
     return Generator.prototype.help.apply(subgen);
   }
 
   prompting () {
-    if (this.bail) return;
+    this.out.warn(`This sub-generator is deprecated, delegating to ${chalk.yellow('yo alfresco:module-add-source')} instead`);
 
-    if (this.usingEnhancedAlfrescoMavenPlugin) {
-      this.out.warn([
-        'This sub-generator is deprecated starting from SDK 3,',
-        'please use ' + chalk.yellow('yo alfresco:module-add-source'),
-        'instead'].join(' '));
-    }
-
-    return this.subgeneratorPrompt(this.prompts, props => {
-      debug(`delegating to ${DELEGATED_NAMESPACE}`);
+    return this.prompt([]).then(props => {
+      debug(`Delegating to the ${DELEGATED_NAMESPACE} generator`);
       this.composeWith(DELEGATED_NAMESPACE, this.options);
     }).then(() => {
-      debug('prompting finished');
+      debug('Prompting finished');
     });
   }
 }
