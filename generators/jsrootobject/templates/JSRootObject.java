2bfa45e04970edfe291a29c7192198733855575c
Vijay Prince <vprince@ziaconsulting.com>
2018-05-01 13:18:34 -0600

    Added references to Rhinoscript in generated Java class (#192)

--- generators/jsrootobject/templates/JSRootObject.java
+++ generators/jsrootobject/templates/JSRootObject.java
@@ -11,6 +11,13 @@ import org.alfresco.repo.processor.BaseProcessorExtension;
  *
  * http://docs.alfresco.com/5.2/references/dev-extension-points-javascript-root-objects.html
  *
+ * The Rhino JavaScript runtime is really clever at being able to call Java from JavaScript. 
+ * It will automatically handle some type conversions and even allows for javascript functions 
+ * to be passed to java code if the Java code expects an interface with a single method in it, 
+ * the javascript function will be made to look like that function. More details can be found in
+ *
+ * https://developer.mozilla.org/en-US/docs/Mozilla/Projects/Rhino/Scripting_Java#JavaScript_Functions_as_Java_Interfaces
+ *
  */
 public class <%- className %> extends BaseProcessorExtension {
   private static Log logger = LogFactory.getLog(<%- className %>.class);
